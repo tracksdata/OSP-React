117a2db2ec073729436053cfe81f0ec1ab35e381
ctsjava <ctsgit@gmail.com>
2020-04-27 10:07:38 +0530

    react

--- 3-react/js-clock-app/index.js
+++ 3-react/js-clock-app/index.js
@@ -1,41 +1,74 @@
-console.log('index js file');
 
+// Plain JS
+//------------------
 
+let indianClock = document.querySelector("#asia_kolkata");
+let dubaiClock = document.querySelector("#asia_dubai")
+let americaClock = document.querySelector("#america_newyork")
 
-function jsTick() {
-    let jsClock = document.querySelector("#js-clock");
-    fetch('https://jsonplaceholder.typicode.com/users') // model
-        .then(response => response.json())
-        .then((mydata) => {
-            // console.log(mydata);
-            mydata.map(data => {
-                jsClock.innerHTML = JSON.stringify(data); // view
+function clock(props) {
 
-            })
-        
-            //jsClock.innerHTML = mydata;
-
-        })
-        .catch(error=>{
-            console.log(error);
-            
-        })
-
-
-    
-
-    let time = new Date().toLocaleTimeString();
-    let ui = `
-        <div>
-             <span class='badge badge-primary'> ${time}</span>
-        </div>
-
+    // new Date().toLocaleTimeString
+    return (
         `
-    // jsClock.innerHTML = ui;
-
+     <div class="card">
+        <p class="card-header"> ${props.timeZone} </p>
+        <p class="card-body">
+        <span class='badge badge-danger'>
+        ${new Date().toLocaleTimeString('es-US', { timeZone: props.timeZone })} 
+        </span>
+        
+        </p>
+     </div>
+    
+    `)
 }
 
-//setInterval(jsTick,1000);
+setInterval(()=>{
+indianClock.innerHTML = clock({ timeZone: 'Asia/Kolkata' })
+dubaiClock.innerHTML = clock({ timeZone: 'Asia/Dubai' })
+americaClock.innerHTML = clock({ timeZone: 'America/New_york' })
+},1000)
 
 
+
+// Reat JS
+//-----------------
+
+let indianClockV2 = document.querySelector("#asia_kolkata_v2");
+let dubaiClockV2 = document.querySelector("#asia_dubai_v2")
+let americaClockV2 = document.querySelector("#america_newyork_v2")
+
+//function newClock(props){
+// let btnEle=React.createElement('button',{className:"btn btn-primary"},'Click Here');
+//indianClock.innerHTML=btnEle;
+//  console.log(btnEle);
+
+// return btnEle;
+
+//}
+
+
+//ReactDOM.render(newClock(),indianClockV2);
+
+function newClock(props) {
+    // JSX
+    return (
+        <div class="card">
+            <p class="card-header"> {props.timeZone} </p>
+            <p class="card-body">
+                <span class="badge badge-success">
+                {new Date().toLocaleTimeString('es-US', { timeZone: props.timeZone })}    
+                </span> 
+            </p>
+        </div>
+
+    )
+}
+
+setInterval(()=>{
+    ReactDOM.render(newClock({ timeZone: 'Asia/Kolkata' }), indianClockV2)
+    ReactDOM.render(newClock({ timeZone: 'Asia/Dubai' }), dubaiClockV2)
+    ReactDOM.render(newClock({ timeZone: 'America/New_york' }), americaClockV2)
+},1000)
 
