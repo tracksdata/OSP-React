85a1e3b8b7c8bc57ccb037c54a96bcd1ab6a9d49
ctsjava <ctsgit@gmail.com>
2020-05-13 08:39:00 +0530

    react assignment

---
+++ 3-react/11-life-cycle/src/components/Component1.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Component1 extends Component {
+
+    render() {
+        let 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Component1;
---
+++ Assignments/react-exercise/src/components/Instructions.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './../styles/instructions.css'
+
+class Instructions extends Component {
+  render() {
+    return (
+      <div className="instructions">
+        Follow the instructions on the README to get started!
+      </div>
+    )
+  }
+}
+
+export default Instructions
